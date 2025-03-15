9702b9c2999f692df946df21d46434e3e7fe2a47
pbk <impbk2002@gmail.com>
2025-03-15 11:31:18 +0900

    chage main view

--- MacAnimatedImageConverter/AnimatedImageConverterApp.swift
+++ MacAnimatedImageConverter/AnimatedImageConverterApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AnimatedImageConverterApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ImageDropView()
         }
     }
 }
