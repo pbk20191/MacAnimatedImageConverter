094efe10c726dc6037c4b084e4aecc95bbdea6e0
pbk <impbk2002@gmail.com>
2025-03-15 11:13:47 +0900

    Initial Commit

---
+++ AnimatedImageConverter/AnimatedImageConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedImageConverterApp.swift
+//  AnimatedImageConverter
+//
+//  Created by 박병관 on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedImageConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
